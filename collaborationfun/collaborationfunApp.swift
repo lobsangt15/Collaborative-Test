8cb5ce1a7761020ff8ed8c9da0e41c192d755c11
lobsangt15 <lobsangt15@nycstudents.net>
2026-04-23 09:16:49 -0400

    Initial Commit

---
+++ collaborationfun/collaborationfunApp.swift
@@ -0,0 +1,17 @@
+//
+//  collaborationfunApp.swift
+//  collaborationfun
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct collaborationfunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
